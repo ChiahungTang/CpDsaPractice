54b8eefc4444876f3c869ee5ce190bd606505b8c
Chiahung Tang <chaihung.tang0579@gmail.com>
2024-10-31 20:12:32 +0800

    Add Sieve

---
+++ cpdsa/Sieve.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int SIZE = 1e6;
+bool tb[SIZE];
+
+void sieve(vector<long long> &v) {
+  for (long long i = 2; i * i <= SIZE; i++) {
+    if (tb[i] == 0) {
+      v.push_back(i);
+      for (long long j = i * i; j * j <= SIZE; j += i) {
+        tb[j] = 1;
+      }
+    }
+  }
+}
+
+int main() {
+  vector<long long> prime;
+  sieve(prime);
+
+  for (int i = 0; i < (int)prime.size(); i++) {
+    cout << prime[i] << ' ';
+  }
+  cout << endl;
+
+  return 0;
+}
